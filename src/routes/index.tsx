b2fa2c944a4f8f3e03782526d29bffc9a0d3e2b7
dev-mauricioAB <dev.mauricioalezandre@gmail.com>
2020-07-25 20:41:00 -0300

    First commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
+
+import SignIn from '../pages/SingIn';
+import SignUp from '../pages/SingUp';
+import Dashboard from '../pages/Dashboard';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={SignIn} />
+    <Route path="/signup" exact component={SignUp} />
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
+  </Switch>
+);
+
+export default Routes;
